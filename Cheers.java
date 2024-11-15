09ca855e1f38a8f6199aa4798af98e3be1ed3f8f
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-15 21:12:10 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -22,6 +22,6 @@ public class Cheers {
                         System.out.println(cheer + "!!!");
                         i++;
                 }
-
+                System.out.println();
         }
 }
